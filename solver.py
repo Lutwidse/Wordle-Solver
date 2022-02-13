76f0f10f92bc430ae01e5a4fe337915f87c28cce
Lutwidse <74334594+Lutwidse@users.noreply.github.com>
2022-02-14 03:23:04 +0900

    fix unintended handling of the word validation

--- solver.py
+++ solver.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 def get_words():
     with open("wordle-answers-alphabetical.txt", "r") as f:
         words = f.read().splitlines()
@@ -7,7 +9,7 @@ def get_result():
     result = input("(M/W/C) > ")
     if result == "CCCCC":
         print("(Wordle) > End")
-        exit(1)
+        exit(0)
     return result
 
 def get_guess():
@@ -15,41 +17,37 @@ def get_guess():
     return guess
 
 def update_words(words, guess, result):
-    m,w,c = [],[],[]
+    miss, wrong_spot, correct = [], {}, {}
     for i in range(5):
         if result[i] == "M":
-            m.append(f"{i}:{guess[i]}")
+            miss.append(guess[i])
         elif result[i] == "W":
-            w.append(guess[i])
+            wrong_spot.update({i:guess[i]})
         elif result[i] == "C":
-            c.append(f"{i}:{guess[i]}")
+            correct.update({i:guess[i]})
 
     if guess in words:
         words.remove(guess)
     updated_words = []
+
+    # Miss
     for i in words:
-        # Miss
-        for j in m:
-            idx, char = j.split(":")
-            if i[int(idx)] == char:
+        for j in miss:
+            if j in i and j not in wrong_spot.values():
                 break
         else:
-            # Correct
-            for k in c:
-                idx, char = k.split(":")
-                if i[int(idx)] != char:
-                    break
-            else:
-                # WrongSpot
-                for l in w:
-                    if l not in i:
-                        break
-                else:
+            updated_words.append(i)
+
+    # WrongSpot and Correct
+    for i in words: 
+        for ws_k, ws_v in wrong_spot.items():
+            for c_k, c_v in correct.items():
+                if i[ws_k] != c_k or i[ws_v] not in c_v:
                     updated_words.append(i)
 
     if not updated_words:
         print("(Wordle) > Words are empty")
-        exit(1)
+        exit(0)
     return updated_words
             
 
